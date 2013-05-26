puppetlabs/puppetlabs-concat

commit 4396d601d61effe987594a4a272882ea201f35ed

Move away from defining resource defaults, we can just pass $source and $content directly to the file resource
Using the case statement does not properly notify the file resource about
changes, leading to unexpected results when generating fragments from Puppet
templates. This patch fixes that issue.

manifests/fragment.pp
@@ -13,7 +13,7 @@
 #   - group     Owner of the file
 #   - backup    Controls the filebucketing behavior of the final file and
 #               see File type reference for its use.  Defaults to 'puppet'
-define concat::fragment($target, $content='', $source='', $order=10, $ensure = 'present', $mode = '0644', $owner = $::id, $group = $concat::setup::root_group, $backup = 'puppet') {
+define concat::fragment($target, $content=undef, $source=undef, $order=10, $ensure = 'present', $mode = '0644', $owner = $::id, $group = $concat::setup::root_group, $backup = 'puppet') {
   $safe_name = regsubst($name, '[/\n]', '_', 'GM')
   $safe_target_name = regsubst($target, '[/\n]', '_', 'GM')
   $concatdir = $concat::setup::concatdir
@@ -21,32 +21,26 @@ define concat::fragment($target, $content='', $source='', $order=10, $ensure = '
 
   # if content is passed, use that, else if source is passed use that
   # if neither passed, but $ensure is in symlink form, make a symlink
-  case $content {
-    '': {
-      case $source {
-        '': {
-          case $ensure {
-            '', 'absent', 'present', 'file', 'directory': {
-              crit('No content, source or symlink specified')
-            }
-            default: {
-              #do nothing, make puppet-lint happy.
-            }
-          }
-        }
-        default: { File{ source => $source } }
+  case $ensure {
+    '', 'absent', 'present', 'file', 'directory': {
+      if ! ($content or $source) {
+        crit('No content, source or symlink specified')
       }
+    },
+    default: {
+      # do nothing, make puppet-lint happy
     }
-    default: { File{ content => $content } }
   }
 
   file{"${fragdir}/fragments/${order}_${safe_name}":
-    ensure => $ensure,
-    mode   => $mode,
-    owner  => $owner,
-    group  => $group,
-    backup => $backup,
-    alias  => "concat_fragment_${name}",
-    notify => Exec["concat_${target}"]
+    ensure  => $ensure,
+    mode    => $mode,
+    owner   => $owner,
+    group   => $group,
+    source  => $source,
+    content => $content,
+    backup  => $backup,
+    alias   => "concat_fragment_${name}",
+    notify  => Exec["concat_${target}"]
   }
 }

Arakmar/puppet-concat

commit 9222f46aa5fa94ae54463935be75701ead472edc

Fix a major regression introduced by 4396d601d61effe987 Did this patch was tested ?!

manifests/fragment.pp
@@ -35,33 +35,39 @@ define concat::fragment(
     $owner = $::id,
     $group = $concat::setup::root_group,
     $backup = 'puppet') {
-  $safe_name = regsubst($name, '[/\n]', '_', 'GM')
-  $safe_target_name = regsubst($target, '[/\n]', '_', 'GM')
-  $concatdir = $concat::setup::concatdir
-  $fragdir = "${concatdir}/${safe_target_name}"
+    $safe_name = regsubst($name, '[/\n]', '_', 'GM')
+    $safe_target_name = regsubst($target, '[/\n]', '_', 'GM')
+    $concatdir = $concat::setup::concatdir
+    $fragdir = "${concatdir}/${safe_target_name}"
 
-  # if content is passed, use that, else if source is passed use that
-  # if neither passed, but $ensure is in symlink form, make a symlink
-  case $ensure {
-    '', 'absent', 'present', 'file', 'directory': {
-      if ! ($content or $source) {
-        crit('No content, source or symlink specified')
-      }
-    }
-    default: {
-      # do nothing, make puppet-lint happy
-    }
-  }
+    if $ensure == 'present' {
+        if $source == '' and $content == undef {
+            fail("One of \$source or \$content must be specified")
+        }
 
-  file{"${fragdir}/fragments/${order}_${safe_name}":
-    ensure  => $ensure,
-    mode    => $mode,
-    owner   => $owner,
-    group   => $group,
-    source  => $source,
-    content => $content,
-    backup  => $backup,
-    alias   => "concat_fragment_${name}",
-    notify  => Exec["concat_${target}"]
-  }
+        if $source != '' and $content != undef {
+            fail("Only one of \$source or \$content must be specified")
+        }
+    }
+
+    file{"${fragdir}/fragments/${order}_${safe_name}":
+        ensure  => $ensure,
+        mode    => $mode,
+        owner   => $owner,
+        group   => $group,
+        backup  => $backup,
+        alias   => "concat_fragment_${name}",
+        notify  => Exec["concat_${target}"]
+    }
+
+    if $source {
+        File["${fragdir}/fragments/${order}_${safe_name}"] {
+            source => $source,
+        }
+    }
+    else {
+        File["${fragdir}/fragments/${order}_${safe_name}"] {
+            content => $content,
+        }
+    }
 }